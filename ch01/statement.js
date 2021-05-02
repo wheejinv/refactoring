ae4eebf9d50420d75511f37a906a37b24e8d0020
whee <wheejinv@naver.com>
2021-05-02 12:37:49 +0900

    첫번재 예시

---
+++ ch01/statement.js
@@ -0,0 +1,66 @@
+import plays from "./plays.json"
+import invoices from "./invoices.json"
+
+// 공연료 청구서를 출력하는 코드.
+function statement (invoice, plays) {
+	let totalAmount = 0
+	let volumeCredits = 0
+	let result = `청구 내역 (고객명: ${invoice.customer})\n`
+
+	// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+	const format = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2,
+	}).format;
+
+	for (let perf of invoice.performances) {
+		const play = plays[perf.playID]
+		let thisAmount = 0
+
+		switch (play.type) {
+			case 'tragedy':
+				thisAmount = 40000
+				if (perf.audience > 30) {
+					thisAmount += 1000 * (perf.audience - 30)
+				}
+				break
+			case 'comedy':
+				thisAmount = 30000
+				if (perf.audience > 20) {
+					thisAmount += 10000 + 500 * (perf.audience - 20)
+				}
+				thisAmount += 300 * perf.audience
+				break
+			default:
+				throw new Error(`알 수 없는 장르: ${play.type}`)
+		}
+
+		// 포인트를 적립한다.
+		volumeCredits += Math.max(perf.audience - 30, 0);
+
+		// 희극 관객 5명마다 추가 포인트를 제공한다.
+		if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+		// 청구 내역을 출력한다.
+		result += `${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`
+		totalAmount += thisAmount
+	}
+
+	result += `총액: ${format(totalAmount/100)}\n`;
+	result += `적립 포인트: ${volumeCredits}점\n`;
+	return result;
+}
+
+console.log(statement(invoices[0], plays));
+
+// 실행: node --experimental-json-modules statement
+
+/* 결과
+청구 내역 (고객명: BigCo)
+Hamlet: $650.00 (55석)
+As You Like It: $580.00 (35석)
+Othello: $500.00 (40석)
+총액: $1,730.00
+적립 포인트: 47점
+*/
