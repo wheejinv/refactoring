4c4a19012ddffc6ff87a5a91680998d733777b77
whee <wheejinv@naver.com>
2021-05-02 23:47:26 +0900

    함수 추출하기 변수의 이름 명확하게 변경 thisAmount → result 첫 번째 인수인 perf → aPerformance 로 리팩터링 amountFor 함수에서 play 변수 제거하기. 변수 인라인하기(6.4절) 적용 함수 선언 바꾸기

--- ch01/statement.js
+++ ch01/statement.js
@@ -1,8 +1,35 @@
-import plays from "./plays.json"
-import invoices from "./invoices.json"
+import plays from './plays.json';
+import invoices from './invoices.json';
 
-// 공연료 청구서를 출력하는 코드.
-function statement (invoice, plays) {
+function statement (invoice) {
+	function amountFor(aPerformance) {
+		let result = 0;
+
+		switch (playFor(aPerformance).type) {
+			case 'tragedy':
+				result = 40000;
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30);
+				}
+				break;
+			case 'comedy':
+				result = 30000;
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
+				}
+				result += 300 * aPerformance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+		}
+
+		return result;
+	}
+
+	function playFor(aPerformance) {
+		return plays[aPerformance.playID];
+	}
+
 	let totalAmount = 0
 	let volumeCredits = 0
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`
@@ -15,36 +42,15 @@ function statement (invoice, plays) {
 	}).format;
 
 	for (let perf of invoice.performances) {
-		const play = plays[perf.playID]
-		let thisAmount = 0
-
-		switch (play.type) {
-			case 'tragedy':
-				thisAmount = 40000
-				if (perf.audience > 30) {
-					thisAmount += 1000 * (perf.audience - 30)
-				}
-				break
-			case 'comedy':
-				thisAmount = 30000
-				if (perf.audience > 20) {
-					thisAmount += 10000 + 500 * (perf.audience - 20)
-				}
-				thisAmount += 300 * perf.audience
-				break
-			default:
-				throw new Error(`알 수 없는 장르: ${play.type}`)
-		}
-
 		// 포인트를 적립한다.
 		volumeCredits += Math.max(perf.audience - 30, 0);
 
 		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+		if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
 		// 청구 내역을 출력한다.
-		result += `${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`
-		totalAmount += thisAmount
+		result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`
+		totalAmount += amountFor(perf)
 	}
 
 	result += `총액: ${format(totalAmount/100)}\n`;
