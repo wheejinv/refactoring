d4d6665e791c613d275e4f3b32b2de1459206cf2
whee <wheejinv@naver.com>
2021-05-06 02:14:48 +0900

    폴더 위치 변경

--- test/ch04/index.js
+++ test/ch04/index.js
@@ -1,5 +1,5 @@
-import Province from "../../ch04/Province";
-import sampleProvinceData from "../../ch04";
+import Province from "../../src/ch04/Province";
+import sampleProvinceData from "../../src/ch04";
 
 var chai = require('chai'); // 자바스크립트용 어서션 라이브러리
 var assert = chai.assert;
