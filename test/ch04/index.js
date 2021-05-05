574180aa8fa85a52eb5e246b84a5fe7a9e0c24c5
whee <wheejinv@naver.com>
2021-05-06 01:48:00 +0900

    4 #4 beforeEach 블록에서 '설정'한 표준 픽스처를 취해서, 테스트를 '수행' 하고, 이 픽스처가 일을 기대한 대로 처리했는지를 '검증'

--- test/ch04/index.js
+++ test/ch04/index.js
@@ -3,6 +3,7 @@ import sampleProvinceData from "../../ch04";
 
 var chai = require('chai'); // 자바스크립트용 어서션 라이브러리
 var assert = chai.assert;
+var expect = chai.expect;
 
 describe('province', function() {
 	let asia;
@@ -11,5 +12,10 @@ describe('province', function() {
 	});
 	it('shortfall', function() {
 		assert.equal(asia.shortfall, 5); // 검증
+	})
+	it('change production', function() {
+		asia.producer[0].production = 20;
+		expect(asia.shortfall).equal(-6);
+		expect(asia.profit).equal(292);
 	})
 })
