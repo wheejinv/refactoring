7b7b5d897553d51e244e4d84c471ab1f93d064dc
whee <wheejinv@naver.com>
2021-05-06 02:00:46 +0900

    4 #5 테스트 추가 - 경계 조건 검사하기 - no producers 상황

--- test/ch04/index.js
+++ test/ch04/index.js
@@ -19,3 +19,19 @@ describe('province', function() {
 		expect(asia.profit).equal(292);
 	})
 })
+
+describe('no producers', function() {
+	let noProducers;
+	beforeEach(function () {
+		const data = {
+			name: "No producers",
+			producers: [],
+			demand: 30,
+			price: 20,
+		};
+		noProducers = new Province(data);
+	});
+	it('shortfall', function () {
+		expect(noProducers.shortfall).equal(30);
+	})
+})
