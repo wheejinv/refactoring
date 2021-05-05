2810bb50213cee21a602bfe7b616fae7877efe33
whee <wheejinv@naver.com>
2021-05-06 01:43:13 +0900

    4 #3 beforeEach 를 사용하여 자신만의 새로운 픽스처를 가지도록 설정

--- test/ch04/index.js
+++ test/ch04/index.js
@@ -1,11 +1,15 @@
 import Province from "../../ch04/Province";
 import sampleProvinceData from "../../ch04";
 
-var assert = require('assert');
+var chai = require('chai'); // 자바스크립트용 어서션 라이브러리
+var assert = chai.assert;
 
 describe('province', function() {
+	let asia;
+	beforeEach(function() {
+		asia = new Province(sampleProvinceData()); // 픽스처 설정
+	});
 	it('shortfall', function() {
-		const asia = new Province(sampleProvinceData()); // 픽스처 설정
 		assert.equal(asia.shortfall, 5); // 검증
 	})
 })
